8e4ae7c17817383eb143c3b8f3e599dfe1e9c1e8
Penguin <penguin@Penguins-MacBook-Air.local>
2017-07-17 17:42:01 +1200

    Adding existing files

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	//Add new item to the list
+	$('#add-list-item').click(function(){
+		var item = $('#new-list-item').val();
+		var activityHTML = '<div class="">';
+		activityHTML += item + '</div>';
+		console.log(activityHTML);
+		$('.selectable').append(activityHTML);
+		$('#new-list-item').val("");
+
+		//Selected item ton mouse in 
+		$('.selectable div').mouseenter(function(){
+			$(this).addClass('activity');
+			$('.activity').click(function(){
+				$(this).toggleClass('selected');
+				//remove selected item 
+				$('.remove').click(function(){
+				$	('.selected').remove();
+				});// end remove item
+			});// end selected item
+		}); // end mouse in 
+	}); //end of add item 
+
+	//deselect item on mouse out
+		$('.selectable div').mouseout(function(){
+			$(this).removeClass('selected');
+		});	
+
+		//move selected item
+		$('#sortable').sortable();
+
+	//hide/show list when clicked on 'Hide/Show List'  button
+	$('#toggle').click(function (){
+		$('.list').toggle("slow");
+	}); //end hide/show list
+
+}); //end document ready
