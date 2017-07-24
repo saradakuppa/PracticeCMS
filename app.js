840294422303a014fa9868ffaeffdfa83eba9da3
Penguin <penguin@Penguins-MacBook-Air.local>
2017-07-24 14:49:34 +1200

    edit, remove and sort working

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	//Add new item to the list
 	$('#add-list-item').click(function(){
 		var item = $('#new-list-item').val();
-		var activityHTML = '<div class="">';
+		var activityHTML = '<div class="" contenteditable="true">';
 		activityHTML += item + '</div>';
 		console.log(activityHTML);
 		$('.selectable').append(activityHTML);
@@ -16,23 +16,46 @@ $(document).ready(function(){
 
 		//Selected item ton mouse in 
 		$('.selectable div').mouseenter(function(){
-			$(this).addClass('activity');
+			$(this).toggleClass('activity');
 			$('.activity').click(function(){
 				$(this).toggleClass('selected');
+
 				//remove selected item 
 				$('.remove').click(function(){
-				$	('.selected').remove();
+					$('.selected').remove();
 				});// end remove item
+
+				//Edit item
+				function editItem(){
+					var divHtml = $(this).html();
+				    var editableText = $("<textarea />");
+				    editableText.val(divHtml);
+				    $(this).replaceWith(editableText);
+				    editableText.focus();
+				    // setup the blur event for this new textarea
+				    editableText.blur(editableTextBlurred);
+				}
+
+				function editableTextBlurred() {
+				    var html = $(this).val();
+				    var viewableText = $("<div>");
+				    viewableText.html(html);
+				    $(this).replaceWith(viewableText);
+				    // setup the click event for this new div
+				    $(viewableText).dblclick(editItem);
+				}
+
+				$('.selected').dblclick(editItem);
 			});// end selected item
 		}); // end mouse in 
 	}); //end of add item 
 
-	//deselect item on mouse out
-		// $('.selectable div').mouseout(function(){
-		// 	$(this).removeClass('selected');
-		// });	
+	
 
-		//move selected item
+
+
+
+	//move selected item
 		$('#sortable').sortable();
 
 	//hide/show list when clicked on 'Hide/Show List'  button
